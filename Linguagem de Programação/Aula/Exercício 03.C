c0b9cfc7f713703b87ef1eea721982b186927fe8
Augustolima-3 <84147801+Augustolima-3@users.noreply.github.com>
2023-03-23 20:39:04 -0300

    Create Exercício 03.C

---
+++ Linguagem de Programação/Aula/Exercício 03.C
@@ -0,0 +1,58 @@
+#include <stdio.h>
+int main(){
+    int matriz [3][3] = 0;
+    int linha, coluna = 0;
+    int soma = 0;
+    
+    printf("\n");
+    for (linha = 0; linha < 3; linha ++){
+        printf("\n");
+         for (coluna = 0; coluna < 3; coluna ++){
+        printf("%d - ", matriz[linha][coluna]);
+        }
+    }
+    //letra A
+    printf("\n");
+    int soma = 0;
+    for(linha = 0; linha < 3; linha++){
+        for(coluna = 0; coluna < 3; coluna++){
+            soma = soma + matriz[linha][coluna];
+        }
+
+    }
+    printf("a) A soma dos elementos eh = %d", soma);
+    //letra B
+    printf("\n");
+    int soma = 1;
+    for(linha = 0; linha < 3; linha++){
+
+            soma = soma * matriz[linha][linha];
+
+
+    }
+    printf("b) O produto da diaginal principal eh = %d", soma);
+
+    //letra C
+    int maior = matriz [0][0];
+    for (linha = 0; linha < 3; linha++){
+        for(coluna = 0; coluna < 3; coluna++){
+            if(matriz[linha][coluna] > maior){
+                maior = matriz[linha][coluna];
+            }
+        }
+    }
+    printf("c) O maior elemento da matriz eh = %d", maior);
+    
+    //letra D
+
+    int somaColuna = 0;
+    for (coluna = 0; coluna < 3; coluna++){
+        for(linha = 0; linha < 3; linha++){
+            somaColuna += matriz[linha][coluna]; 
+        }
+    }
+    printf("d) Soma dos elementos da coluna eh = %d",coluna+1, somaColuna);
+    
+    
+return 0;
+}
